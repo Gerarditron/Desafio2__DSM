50ba08938e3f3e19381aef69f294e605befcfa8c
Gerardo Velasquez <gerardo.jose2011@yahoo.es>
2023-04-04 17:21:18 -0600

    El inicio del desastre

---
+++ app/src/main/java/sv/edu/udb/pharmtrying_desafio2dsm104/MainActivity.kt
@@ -0,0 +1,11 @@
+package sv.edu.udb.pharmtrying_desafio2dsm104
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
