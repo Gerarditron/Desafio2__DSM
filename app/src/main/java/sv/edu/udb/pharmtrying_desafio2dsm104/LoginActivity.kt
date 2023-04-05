ceef4c407e500b2c64472a89f02f720af0798572
Gerardo Velasquez <gerardo.jose2011@yahoo.es>
2023-04-04 18:17:17 -0600

    Login diseniado

--- app/src/main/java/sv/edu/udb/pharmtrying_desafio2dsm104/LoginActivity.kt
+++ app/src/main/java/sv/edu/udb/pharmtrying_desafio2dsm104/LoginActivity.kt
@@ -3,9 +3,13 @@ package sv.edu.udb.pharmtrying_desafio2dsm104
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
+
+
 }
